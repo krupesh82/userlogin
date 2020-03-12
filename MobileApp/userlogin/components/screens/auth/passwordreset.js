9fe038fbfed10f7659cbffeb7d507d8ae0e450f4
Krupesh <krupesh82@gmail.com>
2020-03-13 00:06:09 +0530

    Password Reset Screen

---
+++ MobileApp/userlogin/components/screens/auth/passwordreset.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
+import { CommonActions } from '@react-navigation/native';
+
+
+export default function PasswordReset({ navigation, route }) {
+    const [ username, setUsername ] = useState('');
+    const [ message, setMessage ] = useState('');
+
+    const { resetPassword } = React.useContext(route.params?.authContext);
+
+    const onNavigationBack = route.params?.onNavigateBack;
+
+    const goBack = () => navigation.dispatch(CommonActions.goBack());
+
+    const setResetMessage = (message) => setMessage(message);
+
+    return (
+        <View>
+            <Text>{ message }</Text>
+            <Text>Username</Text>
+            <TextInput
+                placeholder="Enter your username"
+                value={username}
+                onChangeText={text => setUsername(text)}
+                autoCapitalize = {"none"}
+                autoCompleteType = {"email"}
+                keyboardType = {"email-address"}
+            />
+            <Button onPress={() => {resetPassword({ username, onNavigationBack, goBack, setResetMessage }); }} title="Reset" />
+
+        </View>
+    );
+
+    PasswordReset.navigationOptions = screenProps => ({
+        title: "Reset Password",
+        headerStyle: {
+            backgroundColor : "blue"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 24
+        }
+    });
+    
+}
