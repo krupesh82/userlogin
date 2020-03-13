5f001e13ccad73734bf1329f4261eb7a1108dff5
Krupesh <krupesh82@gmail.com>
2020-03-13 23:36:08 +0530

    account activated confirmation view

--- REST-API/userlogin/userlogin/views.py
+++ REST-API/userlogin/userlogin/views.py
@@ -2,6 +2,9 @@ from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.linkedin_oauth2.views import LinkedInOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
+from django.http import HttpResponse
+from django.shortcuts import render
+import json
 
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
@@ -11,3 +14,7 @@ class GoogleLogin(SocialLoginView):
 
 class LinkedInLogin(SocialLoginView):
     adapter_class = LinkedInOAuth2Adapter
+
+def AccountConfirmed(request):
+    # return HttpResponse(json.dumps({ 'detail': 'Email is confirmed and your account is activated now.' }), 'application/json')
+    return render(request, 'account/account_activated.html', {'status' : 'activated'})
